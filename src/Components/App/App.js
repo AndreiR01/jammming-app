284a3185f4919227775e165a8a11a412579bec1c
Andrei Rusu <andrei89ro27@yahoo.co.uk>
2018-07-15 16:37:17 +0100

    testing18

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -65,8 +65,8 @@ class App extends React.Component {
         <div className="App">
             <SearchBar onSearch={this.search} />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack()}/>
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack()} onSave={this.savePlaylist}
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onSave={this.savePlaylist}
             onNameChange={this.updatePlaylistName} />
           </div>
         </div>
